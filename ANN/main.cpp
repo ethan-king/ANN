f13024a7613b0ad8dbb563f4fc000a5f7d5377d5
Ethan <ethan@ek-macbook-air.local>
2018-11-28 14:09:16 -0500

    Initial Commit

---
+++ ANN/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ANN
+//
+//  Created by Ethan on 11/28/18.
+//  Copyright © 2018 Ethan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
